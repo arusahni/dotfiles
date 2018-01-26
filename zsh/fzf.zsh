d61eb4af40bc3f702e8284bb5cab452426b6bb93
Aru Sahni <arusahni@gmail.com>
2018-01-26 15:16:09 -0500

    Use ripgrep for FZF

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,4 +1,6 @@
-export FZF_DEFAULT_COMMAND='ag -g ""'
+# export FZF_DEFAULT_COMMAND='ag -g ""'
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
+export FZF_DEFAULT_COMMAND='rg --files --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
