3874eb530bfca039dc422eed3a4dd00205057da1
Aru Sahni <aru@arusahni.net>
2022-12-11 18:28:31 -0500

    Add wezterm configuration

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,17 @@
+return {
+    color_scheme = "midnight-in-mojave",
+    colors = {
+        -- Overrides the cell background color when the current cell is occupied by the
+        -- cursor and the cursor style is set to Block
+        cursor_bg = "#fff",
+        -- Specifies the border color of the cursor when the cursor style is set to Block,
+        -- or the color of the vertical or horizontal bar when the cursor style is set to
+        -- Bar or Underline.
+        cursor_border = "#fff",
+        scrollbar_thumb = "#444",
+    },
+    cursor_blink_rate = 1000,
+    enable_scroll_bar = true,
+    tab_bar_at_bottom = true,
+    use_fancy_tab_bar = false,
+  }
