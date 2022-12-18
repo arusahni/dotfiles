f4b43776e48194638fc4698e300f8959c3fbd3db
Aru Sahni <aru@arusahni.net>
2022-12-18 18:02:54 -0500

    Allow for machine-local Wezterm config overrides.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,4 +1,33 @@
-return {
+-- From https://stackoverflow.com/a/15434737/214197
+function isModuleAvailable(name)
+  if package.loaded[name] then
+    return true
+  else
+    for _, searcher in ipairs(package.searchers or package.loaders) do
+      local loader = searcher(name)
+      if type(loader) == 'function' then
+        package.preload[name] = loader
+        return true
+      end
+    end
+    return false
+  end
+end
+
+local merge = function(a, b)
+    local c = {}
+    for k,v in pairs(a) do c[k] = v end
+    for k,v in pairs(b) do c[k] = v end
+    return c
+end
+
+local overrides = {}
+
+if isModuleAvailable("local") then
+    overrides = require("local")
+end
+
+return merge({
     color_scheme = "midnight-in-mojave",
     colors = {
         -- Overrides the cell background color when the current cell is occupied by the
@@ -12,7 +41,6 @@ return {
     },
     cursor_blink_rate = 1000,
     enable_scroll_bar = true,
-    harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
     tab_bar_at_bottom = true,
     use_fancy_tab_bar = false,
-  }
+  }, overrides)
