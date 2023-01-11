7b2a34fb82d361a261c8cdafc18ecad6acc76ab6
Aru Sahni <aru@arusahni.net>
2023-01-11 13:53:19 -0500

    Require ctrl+click for wezterm links

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,3 +1,5 @@
+local wezterm = require("wezterm")
+
 -- From https://stackoverflow.com/a/15434737/214197
 function isModuleAvailable(name)
   if package.loaded[name] then
@@ -41,6 +43,28 @@ return merge({
     },
     cursor_blink_rate = 1000,
     enable_scroll_bar = true,
+    hide_tab_bar_if_only_one_tab = true,
+    mouse_bindings = {
+        -- Disable the default click behavior
+        {
+          event = { Up = { streak = 1, button = "Left"} },
+          mods = "NONE",
+          action = wezterm.action.DisableDefaultAssignment,
+        },
+        -- Ctrl-click will open the link under the mouse cursor
+        {
+            event = { Up = { streak = 1, button = "Left" } },
+            mods = "CTRL",
+            action = wezterm.action.OpenLinkAtMouseCursor,
+        },
+        -- Disable the Ctrl-click down event to stop programs from seeing it when a URL is clicked
+        {
+            event = { Down = { streak = 1, button = "Left" } },
+            mods = "CTRL",
+            action = wezterm.action.Nop,
+        },
+    },
+    scrollback_lines = 99999,
     tab_bar_at_bottom = true,
     use_fancy_tab_bar = false,
   }, overrides)
