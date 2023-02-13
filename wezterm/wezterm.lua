db366871b7c1093009d40a3f3d9f14707c9413fb
Aru Sahni <aru@arusahni.net>
2023-02-13 08:45:16 -0500

    Use nerdfonts for Wezterm tab icons

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -29,6 +29,103 @@ if isModuleAvailable("local") then
     overrides = require("local")
 end
 
+local function get_current_working_dir(tab)
+	local current_dir = tab.active_pane.current_working_dir
+	local HOME_DIR = string.format("file://%s", os.getenv("HOME"))
+
+	return current_dir == HOME_DIR and "~"
+		or string.format("%s", string.gsub(current_dir, "(.*[/\\])(.*)", "%2"))
+end
+
+local function get_process(tab)
+	local process_icons = {
+		["docker"] = {
+			-- { Foreground = { Color = colors.blue } },
+			{ Text = wezterm.nerdfonts.linux_docker },
+		},
+		["docker-compose"] = {
+			-- { Foreground = { Color = colors.blue } },
+			{ Text = wezterm.nerdfonts.linux_docker },
+		},
+		["nvim"] = {
+			-- { Foreground = { Color = colors.green } },
+			{ Text = wezterm.nerdfonts.custom_vim },
+		},
+		["vim"] = {
+			-- { Foreground = { Color = colors.green } },
+			{ Text = wezterm.nerdfonts.dev_vim },
+		},
+		["node"] = {
+			-- { Foreground = { Color = colors.green } },
+			{ Text = wezterm.nerdfonts.mdi_hexagon },
+		},
+		["zsh"] = {
+			-- { Foreground = { Color = colors.peach } },
+			{ Text = wezterm.nerdfonts.dev_terminal },
+		},
+		["bash"] = {
+			-- { Foreground = { Color = colors.subtext0 } },
+			{ Text = wezterm.nerdfonts.cod_terminal_bash },
+		},
+		["pacman"] = {
+			-- { Foreground = { Color = colors.lavender } },
+			{ Text = wezterm.nerdfonts.linux_archlinux },
+		},
+		["htop"] = {
+			-- { Foreground = { Color = colors.yellow } },
+			{ Text = wezterm.nerdfonts.mdi_chart_donut_variant },
+		},
+		["cargo"] = {
+			-- { Foreground = { Color = colors.peach } },
+			{ Text = wezterm.nerdfonts.dev_rust },
+		},
+		["go"] = {
+			-- { Foreground = { Color = colors.sapphire } },
+			{ Text = wezterm.nerdfonts.mdi_language_go },
+		},
+		["docker"] = {
+			-- { Foreground = { Color = colors.blue } },
+			{ Text = wezterm.nerdfonts.linux_docker },
+		},
+		["git"] = {
+			-- { Foreground = { Color = colors.peach } },
+			{ Text = wezterm.nerdfonts.dev_git },
+		},
+		["lua"] = {
+			-- { Foreground = { Color = colors.blue } },
+			{ Text = wezterm.nerdfonts.seti_lua },
+		},
+		["wget"] = {
+			-- { Foreground = { Color = colors.yellow } },
+			{ Text = wezterm.nerdfonts.mdi_arrow_down_box },
+		},
+		["curl"] = {
+			-- { Foreground = { Color = colors.yellow } },
+			{ Text = wezterm.nerdfonts.mdi_flattr },
+		}
+	}
+
+	local process_name = string.gsub(tab.active_pane.foreground_process_name, "(.*[/\\])(.*)", "%2")
+
+	return wezterm.format(
+		process_icons[process_name]
+			or { { Text = string.format("[%s]", process_name) } }
+	)
+end
+
+wezterm.on("format-tab-title", function(tab)
+	return wezterm.format({
+		{ Attribute = { Intensity = "Half" } },
+		{ Text = string.format(" %s ", tab.tab_index + 1) },
+		"ResetAttributes",
+		{ Text = get_process(tab) },
+		{ Text = string.format("  %s", tab.active_pane.title) },
+		-- { Text = get_current_working_dir(tab) },
+		-- { Foreground = { Color = colors.base } },
+		{ Text = "  ▕" },
+	})
+end)
+
 return merge({
     color_scheme = "midnight-in-mojave",
     colors = {
