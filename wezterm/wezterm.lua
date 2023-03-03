662d6c0d4913e9fcf8ac8ab3f0f20d57d64cef82
Aru Sahni <aru@arusahni.net>
2023-03-03 18:53:58 -0500

    Format wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,129 +1,125 @@
 local wezterm = require("wezterm")
 
 -- From https://stackoverflow.com/a/15434737/214197
-function isModuleAvailable(name)
-  if package.loaded[name] then
-    return true
-  else
-    for _, searcher in ipairs(package.searchers or package.loaders) do
-      local loader = searcher(name)
-      if type(loader) == 'function' then
-        package.preload[name] = loader
+local function is_module_available(name)
+    if package.loaded[name] then
         return true
-      end
+    else
+        for _, searcher in ipairs(package.searchers or package.loaders) do
+            local loader = searcher(name)
+            if type(loader) == 'function' then
+                package.preload[name] = loader
+                return true
+            end
+        end
+        return false
     end
-    return false
-  end
 end
 
 local merge = function(a, b)
     local c = {}
-    for k,v in pairs(a) do c[k] = v end
-    for k,v in pairs(b) do c[k] = v end
+    for k, v in pairs(a) do c[k] = v end
+    for k, v in pairs(b) do c[k] = v end
     return c
 end
 
 local overrides = {}
 
-if isModuleAvailable("local") then
+if is_module_available("local") then
     overrides = require("local")
 end
 
 local function get_current_working_dir(tab)
-	local current_dir = tab.active_pane.current_working_dir
-	local HOME_DIR = string.format("file://%s", os.getenv("HOME"))
+    local current_dir = tab.active_pane.current_working_dir
+    local HOME_DIR = string.format("file://%s", os.getenv("HOME"))
 
-	return current_dir == HOME_DIR and "~"
-		or string.format("%s", string.gsub(current_dir, "(.*[/\\])(.*)", "%2"))
+    return current_dir == HOME_DIR and "~"
+        or string.format("%s", string.gsub(current_dir, "(.*[/\\])(.*)", "%2"))
 end
 
 local function get_process(tab)
-	local process_icons = {
-		["docker"] = {
-			-- { Foreground = { Color = colors.blue } },
-			{ Text = wezterm.nerdfonts.linux_docker },
-		},
-		["docker-compose"] = {
-			-- { Foreground = { Color = colors.blue } },
-			{ Text = wezterm.nerdfonts.linux_docker },
-		},
-		["nvim"] = {
-			-- { Foreground = { Color = colors.green } },
-			{ Text = wezterm.nerdfonts.custom_vim },
-		},
-		["vim"] = {
-			-- { Foreground = { Color = colors.green } },
-			{ Text = wezterm.nerdfonts.dev_vim },
-		},
-		["node"] = {
-			-- { Foreground = { Color = colors.green } },
-			{ Text = wezterm.nerdfonts.mdi_hexagon },
-		},
-		["zsh"] = {
-			-- { Foreground = { Color = colors.peach } },
-			{ Text = wezterm.nerdfonts.dev_terminal },
-		},
-		["bash"] = {
-			-- { Foreground = { Color = colors.subtext0 } },
-			{ Text = wezterm.nerdfonts.cod_terminal_bash },
-		},
-		["pacman"] = {
-			-- { Foreground = { Color = colors.lavender } },
-			{ Text = wezterm.nerdfonts.linux_archlinux },
-		},
-		["htop"] = {
-			-- { Foreground = { Color = colors.yellow } },
-			{ Text = wezterm.nerdfonts.mdi_chart_donut_variant },
-		},
-		["cargo"] = {
-			-- { Foreground = { Color = colors.peach } },
-			{ Text = wezterm.nerdfonts.dev_rust },
-		},
-		["go"] = {
-			-- { Foreground = { Color = colors.sapphire } },
-			{ Text = wezterm.nerdfonts.mdi_language_go },
-		},
-		["docker"] = {
-			-- { Foreground = { Color = colors.blue } },
-			{ Text = wezterm.nerdfonts.linux_docker },
-		},
-		["git"] = {
-			-- { Foreground = { Color = colors.peach } },
-			{ Text = wezterm.nerdfonts.dev_git },
-		},
-		["lua"] = {
-			-- { Foreground = { Color = colors.blue } },
-			{ Text = wezterm.nerdfonts.seti_lua },
-		},
-		["wget"] = {
-			-- { Foreground = { Color = colors.yellow } },
-			{ Text = wezterm.nerdfonts.mdi_arrow_down_box },
-		},
-		["curl"] = {
-			-- { Foreground = { Color = colors.yellow } },
-			{ Text = wezterm.nerdfonts.mdi_flattr },
-		}
-	}
+    local process_icons = {
+            ["docker"] = {
+            -- { Foreground = { Color = colors.blue } },
+            { Text = wezterm.nerdfonts.linux_docker },
+        },
+            ["docker-compose"] = {
+            -- { Foreground = { Color = colors.blue } },
+            { Text = wezterm.nerdfonts.linux_docker },
+        },
+            ["nvim"] = {
+            -- { Foreground = { Color = colors.green } },
+            { Text = wezterm.nerdfonts.custom_vim },
+        },
+            ["vim"] = {
+            -- { Foreground = { Color = colors.green } },
+            { Text = wezterm.nerdfonts.dev_vim },
+        },
+            ["node"] = {
+            -- { Foreground = { Color = colors.green } },
+            { Text = wezterm.nerdfonts.mdi_hexagon },
+        },
+            ["zsh"] = {
+            -- { Foreground = { Color = colors.peach } },
+            { Text = wezterm.nerdfonts.dev_terminal },
+        },
+            ["bash"] = {
+            -- { Foreground = { Color = colors.subtext0 } },
+            { Text = wezterm.nerdfonts.cod_terminal_bash },
+        },
+            ["pacman"] = {
+            -- { Foreground = { Color = colors.lavender } },
+            { Text = wezterm.nerdfonts.linux_archlinux },
+        },
+            ["htop"] = {
+            -- { Foreground = { Color = colors.yellow } },
+            { Text = wezterm.nerdfonts.mdi_chart_donut_variant },
+        },
+            ["cargo"] = {
+            -- { Foreground = { Color = colors.peach } },
+            { Text = wezterm.nerdfonts.dev_rust },
+        },
+            ["go"] = {
+            -- { Foreground = { Color = colors.sapphire } },
+            { Text = wezterm.nerdfonts.mdi_language_go },
+        },
+            ["git"] = {
+            -- { Foreground = { Color = colors.peach } },
+            { Text = wezterm.nerdfonts.dev_git },
+        },
+            ["lua"] = {
+            -- { Foreground = { Color = colors.blue } },
+            { Text = wezterm.nerdfonts.seti_lua },
+        },
+            ["wget"] = {
+            -- { Foreground = { Color = colors.yellow } },
+            { Text = wezterm.nerdfonts.mdi_arrow_down_box },
+        },
+            ["curl"] = {
+            -- { Foreground = { Color = colors.yellow } },
+            { Text = wezterm.nerdfonts.mdi_flattr },
+        }
+    }
 
-	local process_name = string.gsub(tab.active_pane.foreground_process_name, "(.*[/\\])(.*)", "%2")
+    local process_name = string.gsub(tab.active_pane.foreground_process_name, "(.*[/\\])(.*)", "%2")
 
-	return wezterm.format(
-		process_icons[process_name]
-			or { { Text = string.format("[%s]", process_name) } }
-	)
+    return wezterm.format(
+        process_icons[process_name]
+        or { { Text = string.format("[%s]", process_name) } }
+    )
 end
 
 wezterm.on("format-tab-title", function(tab)
-	return wezterm.format({
-		{ Attribute = { Intensity = "Half" } },
-		{ Text = string.format(" %s ", tab.tab_index + 1) },
-		"ResetAttributes",
-		{ Text = get_process(tab) },
-		{ Text = string.format("  %s", tab.active_pane.title) },
-		-- { Text = get_current_working_dir(tab) },
-		-- { Foreground = { Color = colors.base } },
-		{ Text = "  ▕" },
-	})
+    return wezterm.format({
+        { Attribute = { Intensity = "Half" } },
+        { Text = string.format(" %s ", tab.tab_index + 1) },
+        "ResetAttributes",
+        { Text = get_process(tab) },
+        { Text = string.format("  %s", tab.active_pane.title) },
+        -- { Text = get_current_working_dir(tab) },
+        -- { Foreground = { Color = colors.base } },
+        { Text = "  ▕" },
+    })
 end)
 
 return merge({
@@ -145,9 +141,9 @@ return merge({
     mouse_bindings = {
         -- Disable the default click behavior
         {
-          event = { Up = { streak = 1, button = "Left"} },
-          mods = "NONE",
-          action = wezterm.action.DisableDefaultAssignment,
+            event = { Up = { streak = 1, button = "Left" } },
+            mods = "NONE",
+            action = wezterm.action.DisableDefaultAssignment,
         },
         -- Ctrl-click will open the link under the mouse cursor
         {
@@ -168,4 +164,4 @@ return merge({
         { name = "dropdown" },
     },
     use_fancy_tab_bar = false,
-  }, overrides)
+}, overrides)
