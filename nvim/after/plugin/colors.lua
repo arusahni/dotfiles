f59fd15139bba230a1ff50df8deceb312c3f72fe
Aru Sahni <aru@arusahni.net>
2023-05-24 21:15:29 -0400

    Tweak diagnostic colors and signs

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -8,8 +8,10 @@ hi ALEWarning ctermbg=yellow ctermfg=black
 hi SyntasticError ctermbg=red ctermfg=black
 hi ALEError ctermbg=red ctermfg=black
 hi CocUnderline term=undercurl
-hi DiagnosticError ctermfg=red guifg=red term=undercurl
-hi DiagnosticWarn ctermfg=yellow guifg=yellow term=undercurl
+hi DiagnosticError ctermfg=red guifg=red term=undercurl cterm=undercurl
+hi DiagnosticUnderlineError guisp=red
+hi DiagnosticWarn ctermfg=yellow guifg=yellow term=undercurl cterm=undercurl
+hi DiagnosticUnderlineWarn guisp=yellow
 hi CocInlayHint guibg=NONE guifg=#6F7378 ctermbg=NONE ctermfg=DarkGray
 
 hi CtrlSpaceSelected term=reverse ctermfg=187  ctermbg=23  cterm=bold
