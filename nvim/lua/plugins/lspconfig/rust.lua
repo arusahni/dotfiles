771dc0bd17c0b46fb683ba163b1a081eb755f608
Aru Sahni <aru@arusahni.net>
2023-05-31 19:26:41 -0400

    Disable LSP highlighting for Rust. Always use Clippy

--- nvim/lua/plugins/lspconfig/rust.lua
+++ nvim/lua/plugins/lspconfig/rust.lua
@@ -4,6 +4,19 @@ function M.setup(settings)
     settings["lsp"].skip_server_setup({ "rust_analyzer" })
     local rust_tools = require('rust-tools')
     rust_tools.setup({
+        server = {
+            on_attach = function(client, _bufnr)
+                -- Disable LSP syntax highlighting
+                client.server_capabilities.semanticTokensProvider = nil
+            end,
+            settings = {
+                ["rust-analyzer"] = {
+                    checkOnSave = {
+                        command = "clippy",
+                    },
+                },
+            },
+        },
         tools = {
             inlay_hints = {
                 parameter_hints_prefix = "",
