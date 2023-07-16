e06ea826e5203a2c895f3e1a0d7937e5ec1ee0f7
Aru Sahni <aru@arusahni.net>
2023-07-15 22:37:04 -0400

    Make LSP lazy

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,93 +1,112 @@
 return {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = 'v2.x',
-    dependencies = {
-        -- LSP Support
-        { "neovim/nvim-lspconfig" },
-        {
-            "williamboman/mason.nvim",
-            build = function()
-                pcall(vim.cmd, "MasonUpdate")
-            end,
-        },
-        { "williamboman/mason-lspconfig.nvim" },
-        { "jose-elias-alvarez/null-ls.nvim" },
 
-        -- Autocompletion
-        { "hrsh7th/nvim-cmp" },
-        { "hrsh7th/cmp-nvim-lsp" },
-        { "hrsh7th/cmp-nvim-lsp-signature-help" },
-        { "L3MON4D3/LuaSnip" },
-
-        -- LSP customizations
-        { "j-hui/fidget.nvim", tag = "legacy" },
-        { "onsails/lspkind.nvim" },
-        { "simrat39/inlay-hints.nvim" },
-        { "tamago324/nlsp-settings.nvim" },
-
-        -- Language customizations
-        { "simrat39/rust-tools.nvim" },
-        { "b0o/schemastore.nvim" },
+    {
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v2.x",
+        lazy = true,
+        config = function()
+            require("lsp-zero.settings").preset({})
+        end
     },
-    config = function(_, _)
-        -- Comment this out for debugging
-        vim.lsp.set_log_level("off")
-        local lspzero = require("plugins.lspconfig.lsp-zero").setup()
-        local lsp = lspzero["lsp"]
-        local cmp_action = lspzero["cmp_action"]
-        lsp.ensure_installed({
-            "tsserver",
-            "lua_ls",
-            "pyright",
-            "bashls",
-            "docker_compose_language_service",
-            "dockerls",
-            "eslint",
-            "html",
-            "marksman",
-            "ruff_lsp",
-            "rust_analyzer",
-            "svelte",
-            "yamlls",
-            "jsonls",
-        })
-        local inlay = require("plugins.lspconfig.inlay").setup()
-        lsp.on_attach(function(client, bufnr)
-            lsp.default_keymaps({
-                buffer = bufnr,
-                preserve_mappings = false,
+    {
+        "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
+        dependencies = {
+            { "L3MON4D3/LuaSnip" },
+            { "onsails/lspkind.nvim" },
+        },
+        config = function()
+            -- Autocompletion
+            local lspzero = require("plugins.lspconfig.lsp-zero").setup()
+            local lsp = lspzero["lsp"]
+            local cmp_action = lspzero["cmp_action"]
+            require("plugins.lspconfig.cmp").setup({ cmp_action = cmp_action })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        cmd = "LspInfo",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            -- LSP Support
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "hrsh7th/cmp-nvim-lsp-signature-help" },
+            {
+                "williamboman/mason.nvim",
+                build = function()
+                    pcall(vim.cmd, "MasonUpdate")
+                end,
+            },
+            { "williamboman/mason-lspconfig.nvim" },
+            { "jose-elias-alvarez/null-ls.nvim" },
+
+            -- LSP customizations
+            { "j-hui/fidget.nvim",                tag = "legacy" },
+            { "simrat39/inlay-hints.nvim" },
+            { "tamago324/nlsp-settings.nvim" },
+
+            -- Language customizations
+            { "simrat39/rust-tools.nvim" },
+            { "b0o/schemastore.nvim" },
+        },
+        config = function(_, _)
+            -- Comment this out for debugging
+            vim.lsp.set_log_level("off")
+            local lspzero = require("plugins.lspconfig.lsp-zero").setup()
+            local lsp = lspzero["lsp"]
+            lsp.ensure_installed({
+                "tsserver",
+                "lua_ls",
+                "pyright",
+                "bashls",
+                "docker_compose_language_service",
+                "dockerls",
+                "eslint",
+                "html",
+                "marksman",
+                "ruff_lsp",
+                "rust_analyzer",
+                "svelte",
+                "yamlls",
+                "jsonls",
             })
-            if client.supports_method "textDocument/documentHighlight" then
-                local group = vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
-                vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-                    group = group,
+            local inlay = require("plugins.lspconfig.inlay").setup()
+            lsp.on_attach(function(client, bufnr)
+                lsp.default_keymaps({
                     buffer = bufnr,
-                    callback = vim.lsp.buf.document_highlight,
+                    preserve_mappings = false,
                 })
-                vim.api.nvim_create_autocmd("CursorMoved", {
-                    group = group,
-                    buffer = bufnr,
-                    callback = vim.lsp.buf.clear_references,
-                })
-            end
-            vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
-                vim.lsp.handlers['signature_help'],
-                { border = 'single', close_events = { "CursorMoved", "BufHidden", "InsertCharPre" } }
-            )
-        end)
-        lsp.set_sign_icons({
-            error = "",
-            warn = "",
-            hint = "",
-            info = "",
-        })
-        require("plugins.lspconfig.fidget")
-        require("plugins.lspconfig.lua").setup({ lsp = lsp })
-        require("plugins.lspconfig.typescript").setup({ inlay = inlay })
-        require("plugins.lspconfig.rust").setup({ inlay = inlay, lsp = lsp })
-        require("plugins.lspconfig.json").setup()
-        require("plugins.lspconfig.null").setup()
-        lsp.setup()
-        require("plugins.lspconfig.cmp").setup({ cmp_action = cmp_action })
-    end
+                if client.supports_method "textDocument/documentHighlight" then
+                    local group = vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
+                    vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+                        group = group,
+                        buffer = bufnr,
+                        callback = vim.lsp.buf.document_highlight,
+                    })
+                    vim.api.nvim_create_autocmd("CursorMoved", {
+                        group = group,
+                        buffer = bufnr,
+                        callback = vim.lsp.buf.clear_references,
+                    })
+                end
+                vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
+                    vim.lsp.handlers['signature_help'],
+                    { border = 'single', close_events = { "CursorMoved", "BufHidden", "InsertCharPre" } }
+                )
+            end)
+            lsp.set_sign_icons({
+                error = "",
+                warn = "",
+                hint = "",
+                info = "",
+            })
+            require("plugins.lspconfig.fidget")
+            require("plugins.lspconfig.lua").setup({ lsp = lsp })
+            require("plugins.lspconfig.typescript").setup({ inlay = inlay })
+            require("plugins.lspconfig.rust").setup({ inlay = inlay, lsp = lsp })
+            require("plugins.lspconfig.json").setup()
+            require("plugins.lspconfig.null").setup()
+            lsp.setup()
+        end
+    }
 }
