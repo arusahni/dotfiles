b6237b5ef11c1f8645bbe7a100e06f5b393dbb9a
Aru Sahni <aru@arusahni.net>
2023-11-27 08:52:11 -0500

    Wezterm config formatting

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -39,63 +39,63 @@ end
 
 local function get_process(tab)
     local process_icons = {
-            ["docker"] = {
+        ["docker"] = {
             -- { Foreground = { Color = colors.blue } },
             { Text = wezterm.nerdfonts.linux_docker },
         },
-            ["docker-compose"] = {
+        ["docker-compose"] = {
             -- { Foreground = { Color = colors.blue } },
             { Text = wezterm.nerdfonts.linux_docker },
         },
-            ["nvim"] = {
+        ["nvim"] = {
             -- { Foreground = { Color = colors.green } },
             { Text = wezterm.nerdfonts.custom_vim },
         },
-            ["vim"] = {
+        ["vim"] = {
             -- { Foreground = { Color = colors.green } },
             { Text = wezterm.nerdfonts.dev_vim },
         },
-            ["node"] = {
+        ["node"] = {
             -- { Foreground = { Color = colors.green } },
             { Text = wezterm.nerdfonts.mdi_hexagon },
         },
-            ["zsh"] = {
+        ["zsh"] = {
             -- { Foreground = { Color = colors.peach } },
             { Text = wezterm.nerdfonts.dev_terminal },
         },
-            ["bash"] = {
+        ["bash"] = {
             -- { Foreground = { Color = colors.subtext0 } },
             { Text = wezterm.nerdfonts.cod_terminal_bash },
         },
-            ["pacman"] = {
+        ["pacman"] = {
             -- { Foreground = { Color = colors.lavender } },
             { Text = wezterm.nerdfonts.linux_archlinux },
         },
-            ["htop"] = {
+        ["htop"] = {
             -- { Foreground = { Color = colors.yellow } },
             { Text = wezterm.nerdfonts.mdi_chart_donut_variant },
         },
-            ["cargo"] = {
+        ["cargo"] = {
             -- { Foreground = { Color = colors.peach } },
             { Text = wezterm.nerdfonts.dev_rust },
         },
-            ["go"] = {
+        ["go"] = {
             -- { Foreground = { Color = colors.sapphire } },
             { Text = wezterm.nerdfonts.mdi_language_go },
         },
-            ["git"] = {
+        ["git"] = {
             -- { Foreground = { Color = colors.peach } },
             { Text = wezterm.nerdfonts.dev_git },
         },
-            ["lua"] = {
+        ["lua"] = {
             -- { Foreground = { Color = colors.blue } },
             { Text = wezterm.nerdfonts.seti_lua },
         },
-            ["wget"] = {
+        ["wget"] = {
             -- { Foreground = { Color = colors.yellow } },
             { Text = wezterm.nerdfonts.mdi_arrow_down_box },
         },
-            ["curl"] = {
+        ["curl"] = {
             -- { Foreground = { Color = colors.yellow } },
             { Text = wezterm.nerdfonts.mdi_flattr },
         }
