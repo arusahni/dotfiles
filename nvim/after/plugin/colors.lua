155a29a67f1371e2a37a95122587d9871da63a9a
Aru Sahni <aru@arusahni.net>
2024-07-20 21:50:51 -0400

    Set LSP Inlay Hint color

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -12,6 +12,7 @@ hi DiagnosticUnderlineError guisp=red
 hi DiagnosticWarn ctermfg=yellow guifg=yellow term=undercurl cterm=undercurl
 hi DiagnosticUnderlineWarn guisp=yellow
 hi CocInlayHint guibg=NONE guifg=#6F7378 ctermbg=NONE ctermfg=DarkGray
+hi LspInlayHint guibg=NONE guifg=#6F7378 ctermbg=NONE ctermfg=DarkGray
 
 hi CtrlSpaceSelected term=reverse ctermfg=187  ctermbg=23  cterm=bold
 hi CtrlSpaceNormal   term=NONE    ctermfg=244  ctermbg=232 cterm=NONE
