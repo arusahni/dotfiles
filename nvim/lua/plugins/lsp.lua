b7b2bf10385ab5dab58e5384ecb4f08b9c727532
Aru Sahni <aru@arusahni.net>
2025-03-07 16:30:37 -0500

    Upgrade lazy plugins

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -9,7 +9,7 @@ return {
         "hrsh7th/nvim-cmp",
         event = "InsertEnter",
         dependencies = {
-            { "L3MON4D3/LuaSnip", version = "v2.*" },
+            { "L3MON4D3/LuaSnip",        version = "v2.*" },
             { "saadparwaiz1/cmp_luasnip" },
             { "onsails/lspkind.nvim" },
             { "hrsh7th/cmp-buffer" },
@@ -17,7 +17,7 @@ return {
         config = function()
             -- Autocompletion
             require("plugins.lspconfig.cmp").setup()
-            require("luasnip.loaders.from_snipmate").lazy_load({paths = "~/.config/nvim/snippets"})
+            require("luasnip.loaders.from_snipmate").lazy_load({ paths = "~/.config/nvim/snippets" })
         end
     },
     {
@@ -47,7 +47,7 @@ return {
             {
                 "mrcjkb/rustaceanvim",
                 version = '^4', -- Recommended
-                lazy = false, -- This plugin is already lazy
+                lazy = false,   -- This plugin is already lazy
             },
             { "b0o/schemastore.nvim" },
             { "aznhe21/actions-preview.nvim" },
@@ -59,21 +59,21 @@ return {
             local lsp = require("plugins.lspconfig.lsp-zero").setup()
             require("mason").setup({})
             require("mason-lspconfig").setup({
-              ensure_installed = {
-                "ts_ls",
-                "lua_ls",
-                "pyright",
-                "bashls",
-                "docker_compose_language_service",
-                "dockerls",
-                "eslint",
-                "html",
-                "marksman",
-                "ruff_lsp",
-                "svelte",
-                "yamlls",
-                "jsonls",
-              }
+                ensure_installed = {
+                    "ts_ls",
+                    "lua_ls",
+                    "pyright",
+                    "bashls",
+                    "docker_compose_language_service",
+                    "dockerls",
+                    "eslint",
+                    "html",
+                    "marksman",
+                    "ruff",
+                    "svelte",
+                    "yamlls",
+                    "jsonls",
+                }
             })
             require("mason-lspconfig").setup_handlers({
                 function(server_name)
@@ -82,7 +82,7 @@ return {
                 end,
                 -- Force no-op for r-a so Meson can't install it
                 rust_analyzer = function()
-                  return true
+                    return true
                 end,
             })
             require("plugins.lspconfig.fidget")
