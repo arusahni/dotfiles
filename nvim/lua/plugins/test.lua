2b3f762fc0fede70b3dcf7bae6b56af9348f1935
Aru Sahni <aru@arusahni.net>
2026-05-21 20:53:44 -0400

    nvim: switch to arborist

--- nvim/lua/plugins/test.lua
+++ nvim/lua/plugins/test.lua
@@ -17,7 +17,7 @@ return {
         dependencies = {
             "nvim-neotest/nvim-nio",
             "nvim-lua/plenary.nvim",
-            "nvim-treesitter/nvim-treesitter",
+            -- "nvim-treesitter/nvim-treesitter",
             "rouge8/neotest-rust",
             "marilari88/neotest-vitest",
         },
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,27 +1,37 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
+    "arborist-ts/arborist.nvim",
     event = { "BufReadPost", "BufNewFile" },
     opts = {
-        indent = {
-            enable = false
-        },
-        ensure_installed = "all", -- one of "all", "maintained", or a list of languages
-        -- List of parsers to ignore installing
-        ignore_install = {
-            "systemverilog",
-        },
-        highlight = {
-            enable = true,  -- enable = true (false will disable the whole extension)
-            disable = {},   -- list of language that will be disabled
-            -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-            -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-            -- Using this option may slow down your editor, and you may see some duplicate highlights.
-            -- Instead of true it can also be a list of languages
-            additional_vim_regex_highlighting = false
-        }
-    },
-    config = function(_, opts)
-        require("nvim-treesitter.configs").setup(opts)
-    end,
+        install_popular = true,
+        config = function(_, opts)
+            require("arborist").setup(opts)
+        end
+    }
 }
+-- return {
+--     "nvim-treesitter/nvim-treesitter",
+--     build = ":TSUpdate",
+--     event = { "BufReadPost", "BufNewFile" },
+--     opts = {
+--         indent = {
+--             enable = false
+--         },
+--         ensure_installed = "all", -- one of "all", "maintained", or a list of languages
+--         -- List of parsers to ignore installing
+--         ignore_install = {
+--             "systemverilog",
+--         },
+--         highlight = {
+--             enable = true,  -- enable = true (false will disable the whole extension)
+--             disable = {},   -- list of language that will be disabled
+--             -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+--             -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+--             -- Using this option may slow down your editor, and you may see some duplicate highlights.
+--             -- Instead of true it can also be a list of languages
+--             additional_vim_regex_highlighting = false
+--         }
+--     },
+--     config = function(_, opts)
+--         require("nvim-treesitter.configs").setup(opts)
+--     end,
+-- }
